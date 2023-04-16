da6dc21f5dcbf0139171045ef23735a34aa069a1
tariq-nasheed <65060738+tariq-nasheed@users.noreply.github.com>
2023-04-16 14:36:39 -0500

    more freaking crap

--- src/hypermania/Objects/Boilerplate/SaveGame.c
+++ src/hypermania/Objects/Boilerplate/SaveGame.c
@@ -2,8 +2,9 @@
 
 SaveRAM* (*SaveGame_GetSaveRAM)(void);
 void (*SaveGame_SaveGameState)(void);
+int32* (*SaveGame_GetDataPtr)(int32 slot, bool32 encore);
 
 SaveRAM* GetSaveRAM_Safe() {
 	if (SaveGame_GetSaveRAM()) return SaveGame_GetSaveRAM();
-	return (SaveRAM*)((globals->saveSlotID == NO_SAVE_SLOT) ? globals->noSaveSlot : SaveGame_GetDataPtr(globals->saveSlotID));
+	return (SaveRAM*)((globals->saveSlotID == NO_SAVE_SLOT) ? globals->noSaveSlot : SaveGame_GetDataPtr(globals->saveSlotID, globals->gameMode == MODE_ENCORE));
 }
--- src/hypermania/Objects/Boilerplate/SaveGame.h
+++ src/hypermania/Objects/Boilerplate/SaveGame.h
@@ -39,10 +39,12 @@ typedef struct {
 
 extern SaveRAM* (*SaveGame_GetSaveRAM)(void);
 extern void (*SaveGame_SaveGameState)(void);
+extern int32* (*SaveGame_GetDataPtr)(int32 slot, bool32 encore);
 extern SaveRAM* GetSaveRAM_Safe(); // this really shouldnt be here but whatever
 
 #define OBJ_SAVE_SETUP \
   IMPORT_PUBLIC_FUNC(SaveGame_GetSaveRAM); \
-  IMPORT_PUBLIC_FUNC(SaveGame_SaveGameState)
+  IMPORT_PUBLIC_FUNC(SaveGame_SaveGameState); \
+  IMPORT_PUBLIC_FUNC(SaveGame_GetDataPtr)
 
 #endif //! OBJ_SAVEGAME_H
