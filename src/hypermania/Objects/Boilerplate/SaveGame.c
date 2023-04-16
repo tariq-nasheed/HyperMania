84817b1c3c22b4e0b00d25d15c78e85b8a0c63dd
tariq-nasheed <65060738+tariq-nasheed@users.noreply.github.com>
2023-04-16 14:38:52 -0500

    lol

--- src/hypermania/Objects/Boilerplate/SaveGame.c
+++ src/hypermania/Objects/Boilerplate/SaveGame.c
@@ -5,6 +5,6 @@ void (*SaveGame_SaveGameState)(void);
 int32* (*SaveGame_GetDataPtr)(int32 slot, bool32 encore);
 
 SaveRAM* GetSaveRAM_Safe() {
-	if (SaveGame_GetSaveRAM()) return SaveGame_GetSaveRAM();
+	if (SaveGame_GetSaveRAM) return SaveGame_GetSaveRAM();
 	return (SaveRAM*)((globals->saveSlotID == NO_SAVE_SLOT) ? globals->noSaveSlot : SaveGame_GetDataPtr(globals->saveSlotID, globals->gameMode == MODE_ENCORE));
 }
