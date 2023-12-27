320882fd510c608513381dded1a4d9dd538d9e61
Berkeley <65060738+tariq-nasheed@users.noreply.github.com>
2023-12-27 14:12:26 -0600

    made SHC logo optional (disabled by default) and integrated it into CMakeLists.txt

--- src/build.sh
+++ src/build.sh
@@ -6,12 +6,5 @@ fi
 cd build
 cmake ../.. --fresh
 make -j8
-cd ../SHCLogo-RSDKv5Decomp
-if [ ! -d build ]; then
-	mkdir build
-fi
-cd build
-cmake .. --fresh
-make -j8
-mv SHCLogo.so ../../..
-cd ../..
+cp *.so ../..
+cd ..
--- src/makereleases.sh
+++ src/makereleases.sh
@@ -1,25 +1,39 @@
 #!/bin/sh
 
+if [ "$1" = "shc" ]; then
+	withSHC=true
+fi
 modname="HyperMania"
 logicname="hypermania"
 filelist="Data mod.ini SETTINGS.md MANUAL.md src/release-files/modSettings.ini src/release-files/HyperManiaSaveData.bin src/release-files/Data/Game/GameConfig.bin"
 releasedir="HyperMania"
 
+if [ -n "$withSHC" ]; then
+	echo "[SHC LOGO ENABLED]"
+fi
+
 BuildMods() {
 	if [ ! -d build ]; then
 		mkdir build
 	fi
-	cd build
-	cmake $@ ../.. --fresh
-	make -j8
-	cd ../SHCLogo-RSDKv5Decomp
-	if [ ! -d build ]; then
-		mkdir build
+
+	args=$@
+	if [ -n "$withSHC" ]; then
+		args="$args -DSHCLOGO=true"
 	fi
+
 	cd build
-	cmake $@ .. --fresh
+	cmake $args ../.. --fresh
 	make -j8
-	cd ../..
+	cd ..
+}
+
+CopyLogicFiles() {
+	copylist="build/$logicname.$1"
+	if [ -n "$withSHC" ]; then
+		copylist="$copylist build/SHCLogo.$1"
+	fi
+	cp $copylist $releasedir
 }
 
 if [ ! -d $releasedir ]; then
@@ -28,17 +42,22 @@ fi
 
 cd ..
 cp -R --target-directory=src/$releasedir $filelist
+if [ -n "$withSHC" ]; then
+	rm src/$releasedir/mod.ini
+	cp src/release-files/SHCmod.ini src/$releasedir
+	mv src/$releasedir/SHCmod.ini src/$releasedir/mod.ini
+fi
 cd src
 
 # UP-TO-DATE MOD LOADER VERSIONS ###############################################
 # linux
 BuildMods
-cp -R build/$logicname.so SHCLogo-RSDKv5Decomp/build/SHCLogo.so $releasedir
+CopyLogicFiles so
 tar czf $modname-linux.tar.gz $releasedir
 rm $releasedir/*.so
 # windows
 BuildMods -DCMAKE_SYSTEM_NAME=Windows
-cp -R build/$logicname.dll SHCLogo-RSDKv5Decomp/build/SHCLogo.dll $releasedir
+CopyLogicFiles dll
 zip -r $modname-windows.zip $releasedir
 rm $releasedir/*.dll
 
@@ -46,15 +65,16 @@ rm $releasedir/*.dll
 cp -R release-files/Data $releasedir
 # linux
 BuildMods -DLEGACY=true
-cp -R build/$logicname.so SHCLogo-RSDKv5Decomp/build/SHCLogo.so $releasedir
+CopyLogicFiles so
 tar czf $modname-linux-legacy.tar.gz $releasedir
 rm $releasedir/*.so
 # windows
 BuildMods -DCMAKE_SYSTEM_NAME=Windows -DLEGACY=true
-cp -R build/$logicname.dll SHCLogo-RSDKv5Decomp/build/SHCLogo.dll $releasedir
+CopyLogicFiles dll
 zip -r $modname-windows-legacy.zip $releasedir
 rm $releasedir/*.dll
 
 
 # CLEANUP ######################################################################
-echo "i'm too afraid to put rm here so CLEAN IT YOURSELF BUM"
+echo "\033[1;33mrelease builds done! removing temporary directory \"$releasedir\"...\033[0m"
+rm -r $releasedir
